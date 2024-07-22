08961d0f3e32b6f17e9f99469cb6adc8d010541b
Sushant Anand <sushantxanand@gmail.com>
2024-07-22 19:15:46 +0530

    third commit for all message and user functionalities

--- chat-app-frontend/src/components/messages/MessageContainer.jsx
+++ chat-app-frontend/src/components/messages/MessageContainer.jsx
@@ -1,22 +1,31 @@
+import { useEffect } from "react";
+import useConversation from "../../zustand/useConversation.js";
 import MessageInput from "./MessageInput.jsx";
 import Messages from "./Messages.jsx" 
 import { TiMessages } from "react-icons/ti";
 
 const MessageContainer = () => {
-	const noChatSelected = true;
+	const {selectedConversation, setSelectedConversation} = useConversation();
+
+	useEffect(() => {
+		// cleanup function (unmounts)
+		return () => setSelectedConversation(null);
+	}, [setSelectedConversation]);
+
 	return (
 		<div className='w-full min-h-72 flex flex-col'>
-			{noChatSelected ? <NoChatSelected /> : 
+			{!selectedConversation ? ( <NoChatSelected /> 
+			) : (
 			<>
 			{/* Header */}
 			<div className='bg-slate-500 px-4 py-2 mb-2'>
-				<span className='text-gray-900 font-bold'> John </span>
+				<span className='text-gray-900 font-bold text-lg '> {selectedConversation.firstName} {selectedConversation.lastName} </span>
 			</div>
 			<Messages />
 			<MessageInput />
 			</>
-			}
-		</div>	
+			)}
+		</div>
 	);
 }
 
@@ -26,7 +35,7 @@ const NoChatSelected = () => {
 	return (
 		<div className='flex items-center justify-center w-full h-full'>
 			<div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-				<p>Welcome 👋 Sushant Anand ❄</p>
+				<p>Welcome Sushant Anand</p>
 				<p>Select a chat to start messaging</p>
 				<TiMessages className='text-3xl md:text-6xl text-center' />
 			</div>
--- chat-app-frontend/src/components/sidebar/Conversations.jsx
+++ chat-app-frontend/src/components/sidebar/Conversations.jsx
@@ -1,25 +1,20 @@
-import Conversation from "./Conversation.jsx";
+import useGetConversations from "../../hooks/useGetConversations";
+import Conversation from "./Conversation";
 
 const Conversations = () => {
-  return (
-    <div className='py-2 flex flex-col h-72 overflow-auto'>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-        <Conversation/>
-    </div>
-  )
-}
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((conversation, idx) => (
+				<Conversation
+					key={conversation._id}
+					conversation={conversation}
+					lastIdx={idx === conversations.length - 1}
+				/>
+			))}
 
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
 export default Conversations;
