b1921548d5a368119c28e108d5d9a606618c5b2e
Sushant Anand <sushantxanand@gmail.com>
2024-07-23 02:35:49 +0530

    deployment

--- chat-app-backend/server.js
+++ chat-app-backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,6 +10,7 @@ import userRoutes from "./routes/user.route.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,10 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     // root route http://localhost:${PORT}/
-//     res.send("Hello World!"); 
-// });
+app.use(express.static(path.join(__dirname, "/chat-app-frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "chat-app-frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
