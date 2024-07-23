3b1b821c0a9dd0eccdd9695bc4dac5255a0e3385
Sushant Anand <sushantxanand@gmail.com>
2024-07-23 14:41:47 +0530

    misc. changes

--- chat-app-frontend/src/context/SocketContext.jsx
+++ chat-app-frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://raven-chat-app.onrender.com", {
+			const socket = io("https://raven-app.netlify.com", {
 				query: {
 					userId: authUser._id,
 				},
